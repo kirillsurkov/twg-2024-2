8b9afcd9dd2ddb3fc0566952e1f22d7f82c90004
kirillsurkov <surkovk20@yandex.ru>
2024-07-12 02:55:26 +0300

    Update ui mod.rs

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,11 +1,19 @@
 use bevy::{ecs::schedule::ScheduleLabel, prelude::*};
 use cards::CardsPlugin;
+use fight_arena_layout::FightArenaLayout;
 use fight_home_layout::FightHomeLayout;
+use game_timer::GameTimerPlugin;
+use hp_mana_bars::HpManaBarsPlugin;
+use layout::LayoutPlugin;
 use players::PlayersPlugin;
 use screen::ScreenPlugin;
 
 pub mod cards;
+pub mod fight_arena_layout;
 pub mod fight_home_layout;
+pub mod game_timer;
+pub mod hp_mana_bars;
+pub mod layout;
 pub mod players;
 pub mod screen;
 
@@ -18,6 +26,27 @@ pub struct UIPlugin;
 
 impl Plugin for UIPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins((ScreenPlugin, PlayersPlugin, CardsPlugin, FightHomeLayout));
+        app.add_plugins((
+            LayoutPlugin,
+            ScreenPlugin,
+            GameTimerPlugin,
+            PlayersPlugin,
+            CardsPlugin,
+            HpManaBarsPlugin,
+            FightHomeLayout,
+            FightArenaLayout,
+        ));
+        app.add_systems(Startup, init);
     }
+}
+
+#[derive(Resource)]
+struct UiAssets {
+    font_comic: Handle<Font>,
+}
+
+fn init(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.insert_resource(UiAssets {
+        font_comic: asset_server.load("comic.ttf"),
+    });
 }
