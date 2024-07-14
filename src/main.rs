7ceda7c297f08377fd51520b617b94d8e8696e80
kirillsurkov <surkovk20@yandex.ru>
2024-07-14 04:34:52 +0300

    Fix UI schedule bug

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-use bevy::{app::MainScheduleOrder, prelude::*};
+use bevy::{app::MainScheduleOrder, prelude::*, window::WindowResolution};
 use bevy_embedded_assets::EmbeddedAssetPlugin;
 use bevy_hanabi::prelude::*;
-use bevy_inspector_egui::quick::WorldInspectorPlugin;
-use bevy_round_ui::prelude::BevyRoundUiDefaultPlugins;
 use component::ComponentsPlugin;
 use hero::HeroesPlugin;
+use iyes_perf_ui::prelude::*;
 use scene::ScenesPlugin;
 use ui::UIPlugin;
 
@@ -18,7 +17,7 @@ mod ui;
 fn main() {
     let mut app = App::new();
 
-    app.add_schedule(Schedule::new(hero::LocalSchedule))
+    app.add_schedule(Schedule::new(ui::LocalSchedule))
         .add_plugins(UIPlugin)
         .world
         .resource_mut::<MainScheduleOrder>()
@@ -42,12 +41,30 @@ fn main() {
         .resource_mut::<MainScheduleOrder>()
         .insert_after(component::LocalSchedule, scene::LocalSchedule);
 
-    app.add_plugins(DefaultPlugins)
-        .add_plugins(HanabiPlugin)
-        .add_plugins(BevyRoundUiDefaultPlugins)
-        .add_plugins(EmbeddedAssetPlugin::default())
-        //.add_plugins(NoCameraPlayerPlugin)
-        // .add_plugins(WorldInspectorPlugin::new())
-        .add_systems(Update, bevy::window::close_on_esc)
-        .run();
+    app.add_plugins(DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(Window {
+            title: "TWG: UNITED (ВСЕ В СБОРЕ)".to_string(),
+            resolution: WindowResolution::new(1600.0, 900.0),
+            ..Default::default()
+        }),
+        ..Default::default()
+    }))
+    .add_plugins(HanabiPlugin)
+    .add_plugins(EmbeddedAssetPlugin::default())
+    .add_plugins((PerfUiPlugin, bevy::diagnostic::FrameTimeDiagnosticsPlugin))
+    // .add_plugins(bevy_inspector_egui::quick::WorldInspectorPlugin::new())
+    .add_systems(Update, bevy::window::close_on_esc)
+    .add_systems(Startup, init)
+    .run();
+}
+
+fn init(mut commands: Commands) {
+    commands.spawn((
+        PerfUiRoot {
+            display_labels: false,
+            layout_horizontal: true,
+            ..Default::default()
+        },
+        PerfUiEntryFPS::default(),
+    ));
 }
